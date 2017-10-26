910e000ae1eea429935e49c2e3d1cb67bda32c5a
MICKY <micky101224@gmail.com>
2017-10-26 22:52:18 +0200

    no message

--- 2DGame/02-Bubble/02-Bubble/Rueda.cpp
+++ 2DGame/02-Bubble/02-Bubble/Rueda.cpp
@@ -5,14 +5,19 @@
 #include <GL/glut.h>
 #include "Game.h"
 
-
+enum PlayerAnims
+{
+	QUIETO, DERECHA, IZQUIERDA
+};
 
 
 void Rueda::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 
-	spritesheet.loadFromFile("images/rueda2.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(64, 48), glm::vec2(1, 1), &spritesheet, &shaderProgram);
+	spritesheet.loadFromFile("images/ruedas.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	sprite = Sprite::createSprite(glm::ivec2(61, 31), glm::vec2(0.25, 0.5), &spritesheet, &shaderProgram);
+	sprite->setNumberAnimations(3);
+
 	tileMapDispl = tileMapPos;
 	sprite->setPosition(glm::vec2(float(tileMapDispl.x + posRueda.x), float(tileMapDispl.y + posRueda.y)));
 
@@ -22,7 +27,7 @@ void Rueda::update(int deltaTime)
 {
 	sprite->update(deltaTime,false);
 	if (Game::instance().getKey(32)) {
-		//hacer que salga una bola
+		
 	}
 
 
