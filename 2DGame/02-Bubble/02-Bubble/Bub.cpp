83db50590a229991ca47edffcf473709661c926f
micky101224 <micky101224@gmail.com>
2017-10-27 12:49:55 +0200

    falta el tubo solo

--- 2DGame/02-Bubble/02-Bubble/Bub.cpp
+++ 2DGame/02-Bubble/02-Bubble/Bub.cpp
@@ -8,7 +8,7 @@
 
 enum PlayerAnims
 {
-	QUIETO, LANZAR_BOLA, COGER_BOLA
+	QUIETO, LANZAR_BOLA, RAPIDO, PARPADEA
 };
 
 void Bub::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
@@ -16,24 +16,30 @@ void Bub::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 	
 	
 	tiempo = 0;
-	spritesheet.loadFromFile("images/bubs2.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	spritesheet.loadFromFile("images/bubs3.png", TEXTURE_PIXEL_FORMAT_RGBA);
 	sprite = Sprite::createSprite(glm::ivec2(45,50), glm::vec2(0.25,0.5), &spritesheet, &shaderProgram);
 	
-	sprite->setNumberAnimations(3);
+	sprite->setNumberAnimations(4);
+	
 	sprite->setAnimationSpeed(QUIETO, 8);
 	sprite->addKeyframe(QUIETO, glm::vec2(0.f, 0.f));
 
-	sprite->setAnimationSpeed(COGER_BOLA, 4);
-	sprite->addKeyframe(COGER_BOLA, glm::vec2(0.25f, 0.f));
-	sprite->addKeyframe(COGER_BOLA, glm::vec2(0.5f, 0.f));
-	sprite->addKeyframe(COGER_BOLA, glm::vec2(0.75f, 0.f)); 
-	sprite->addKeyframe(COGER_BOLA, glm::vec2(0.0f, 0.5f));
+	
+	sprite->setAnimationSpeed(PARPADEA, 8);
+	sprite->addKeyframe(PARPADEA, glm::vec2(0.25f, 0.f));
+	sprite->addKeyframe(PARPADEA, glm::vec2(0.5f, 0.f));
+	sprite->addKeyframe(QUIETO, glm::vec2(0.f, 0.f));
+
+	sprite->setAnimationSpeed(RAPIDO, 8);
+	sprite->addKeyframe(RAPIDO, glm::vec2(0.5f, 0.5f));
+	sprite->addKeyframe(RAPIDO, glm::vec2(0.75f, 0.5f));
+
 	
 	
 	sprite->setAnimationSpeed(LANZAR_BOLA, 10);
+	sprite->addKeyframe(LANZAR_BOLA, glm::vec2(0.75f, 0.0f));
+	sprite->addKeyframe(LANZAR_BOLA, glm::vec2(0.0f, 0.5f));
 	sprite->addKeyframe(LANZAR_BOLA, glm::vec2(0.25f, 0.5f));
-	sprite->addKeyframe(LANZAR_BOLA, glm::vec2(0.50f, 0.5f));
-	sprite->addKeyframe(LANZAR_BOLA, glm::vec2(0.75f, 0.5f));
 
 	
 
@@ -46,7 +52,7 @@ void Bub::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 void Bub::update(int deltaTime)
 {
 	tiempo += deltaTime;
-	sprite->update(deltaTime,false);
+	sprite->update(deltaTime,true);
 	if (sprite->animation() != LANZAR_BOLA && lanzada) {
 		sprite->changeAnimation(LANZAR_BOLA);
 		tiempo = 0;
@@ -58,6 +64,22 @@ void Bub::update(int deltaTime)
 		tiempo = 0;
 
 	}
+	else if ((sprite->animation() == QUIETO || sprite->animation() == PARPADEA) && tiempo > 3000) {
+		sprite->changeAnimation(RAPIDO);
+	}
+	
+	/*else if (sprite->animation() == QUIETO && tiempo > 2000) {
+		sprite->changeAnimation(PARPADEA);
+		if (tiempo > 4000) sprite->changeAnimation(RAPIDO);
+	}
+	*/
+
+	else if (sprite->animation() == PARPADEA && tiempo >= 300) {
+		sprite->changeAnimation(QUIETO);
+		tiempo = 0;
+	}
+	
+	
 
 }
 
