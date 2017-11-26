9055410178af43d2113cc0ad166aa86089c07cf4
wanetty <eduard.gonzalez.moreno@est.fib.upc.edu>
2017-11-26 18:03:55 +0100

    Movimiento jugador y camara.

--- Juego3D/Juego3D/Assets/Scripts/Movimiento_player.cs
+++ Juego3D/Juego3D/Assets/Scripts/Movimiento_player.cs
@@ -8,6 +8,9 @@ public class Movimiento_player : MonoBehaviour {
     float perc = 1;
     Vector3 startPos;
     Vector3 endPos;
+    public bool saltando;
+    bool primero;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,32 +20,45 @@ public class Movimiento_player : MonoBehaviour {
 	void Update () {
         if (Input.GetButtonDown("arriba") || Input.GetButtonDown("abajo") || Input.GetButtonDown("der") || Input.GetButtonDown("izq"))
         {
-            if(perc == 1)
+            if (perc == 1)
             {
                 lerpTime = 1;
                 currentTime = 0;
+                primero = true;
+                saltando = true;
             }
             startPos = gameObject.transform.position;
-            if(Input.GetButtonDown("izq") && gameObject.transform.position == endPos)
-            {
-                endPos = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
-            }
-            if (Input.GetButtonDown("der") && gameObject.transform.position == endPos)
+            if (Input.GetButtonDown("izq") && gameObject.transform.position == endPos)
             {
                 endPos = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
             }
+            if (Input.GetButtonDown("der") && gameObject.transform.position == endPos)
+            {
+                endPos = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
+            }
             if (Input.GetButtonDown("arriba") && gameObject.transform.position == endPos)
             {
-                endPos = new Vector3(transform.position.x , transform.position.y, transform.position.z+1);
+                endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + 1);
             }
             if (Input.GetButtonDown("abajo") && gameObject.transform.position == endPos)
             {
-                endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z-1);
+                endPos = new Vector3(transform.position.x, transform.position.y, transform.position.z - 1);
             }
-            currentTime += Time.deltaTime * 5.5f;
-            perc = currentTime / lerpTime;
-            gameObject.transform.position = Vector3.Lerp(startPos, endPos, perc);
-
         }
+            if (primero)
+            {
+                currentTime += Time.deltaTime * 5f;
+                perc = currentTime / lerpTime;
+                gameObject.transform.position = Vector3.Lerp(startPos, endPos, perc);
+                if(perc > 0.8)
+                {
+                    perc = 1;
+                }
+                if(Mathf.Round(perc) == 1)
+                {
+                    saltando = false;
+                }
+            }
+           
 	}
 }
---
+++ Juego3D/Juego3D/Assets/Scripts/SeguimientoCamara.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeguimientoCamara : MonoBehaviour {
+
+    public GameObject Jugador;
+    Vector3 Posicion;
+	// Update is called once per frame
+	void Update () {
+        Posicion = Vector3.Lerp(gameObject.transform.position, Jugador.transform.position, Time.deltaTime);
+        gameObject.transform.position = new Vector3(Posicion.x, 1, Posicion.z);
+	}
+}
